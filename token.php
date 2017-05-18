e4ff68616b7f9c87769439872917dba117414f6e
Kunihiko Sugiura <kunikunikunikuni@katch.ne.jp>
2017-05-18 17:42:16 +0900

    token有効期限を短く設定

--- token.php
+++ token.php
@@ -7,7 +7,7 @@ $capability = new ClientToken($_ENV['TWILIO_ACCOUNT_SID'], $_ENV['TWILIO_AUTH_TO
 $capability->allowClientOutgoing($_ENV['TWILIO_TWIML_APP_SID']);
 
 // ** みじかければ短い程よい、実用の際は発信直前にtoken取得して有効期限は秒単位にしたほうがいいかも
-$token = $capability->generateToken( 600 );
+$token = $capability->generateToken( 20 );
 
 header('Content-Type: application/json');
 echo json_encode(array(
